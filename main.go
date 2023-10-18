3bcad6ef71e7bb335fe2e30cd471633e422e3344
alekseiKromski <aleksei.kromski@outlook.com>
2023-10-18 20:20:26 +0300

    main.go: implemented basic healthz endpoint

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
-	log.Printf("Ok")
+	if err := godotenv.Load(); err != nil {
+		log.Printf("cannot load env form .env file: %v", err)
+		return
+	}
+	port := os.Getenv("PORT")
+
+	r := gin.Default()
+	r.GET("/healthz", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"status": "OK",
+		})
+	})
+
+	if err := r.Run(fmt.Sprintf(":%s", port)); err != nil {
+		log.Printf("error during running server: %v", err)
+	}
 }
