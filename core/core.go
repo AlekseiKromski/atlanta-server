3eaa2af2eefdd1a8022069f341e8265f6622d197
alekseiKromski <aleksei.kromski@outlook.com>
2024-02-05 23:56:10 +0200

    core: added requirements system and logic for sort dependencies per requirement

--- core/core.go
+++ core/core.go
@@ -23,7 +23,12 @@ func (c *Core) Init(modules []Module) {
 	// Subscribe to Interrupt syscall
 	signal.Notify(c.systemChannel, os.Interrupt)
 
-	c.startModules(modules)
+	// Create requirements tree
+	log.Println("Core: create requirements tree")
+	modules, requirements := c.createRequireTree(modules)
+	log.Println("Core: requirements tree created")
+
+	c.startModules(modules, requirements)
 
 	// Block main thread until stop
 	<-c.systemChannel
@@ -34,16 +39,68 @@ func (c *Core) Init(modules []Module) {
 	os.Exit(0)
 }
 
-func (c *Core) startModules(modules []Module) {
+func (c *Core) startModules(modules []Module, requirements map[string]Module) {
 	startTime := time.Now()
 	log.Printf("Core: Start %d modules", len(modules))
 	for _, module := range modules {
-		go module.Start(c.notifyChannel)
+		var mReqs map[string]Module
+
+		for _, requirement := range module.Require() {
+			mReqs[requirement] = requirements[requirement]
+		}
+
+		go module.Start(c.notifyChannel, mReqs)
 
 		//Wait until module start
 		<-c.notifyChannel
 	}
 	log.Printf("Core: All modules started in %f seconds", time.Now().Sub(startTime).Seconds())
+}
+
+// createRequireTree return a list of sorted modules by requirements
+func (c *Core) createRequireTree(modules []Module) ([]Module, map[string]Module) {
+	requirements := map[string]Module{}
+	var sortedModules []Module
+
+	index := 0
+Main:
+	for {
+		// Fallback to start if index more, that count of  original modules
+		if len(modules) == index {
+			index = 0
+		}
+
+		m := modules[index]
+		signature := m.Signature()
+
+		//Skip already existed modules
+		if requirements[signature] != nil {
+			index++
+			continue
+		}
+
+		mReqs := m.Require()
+		for _, req := range mReqs {
+			// If no requirement, let's process next
+			if requirements[req] == nil {
+				index++
+				continue Main
+			}
+		}
+
+		// Save only not existed
+		requirements[signature] = m
+		sortedModules = append(sortedModules, m)
+
+		// Exit if len of origin modules equals len on sorted
+		if len(modules) == len(sortedModules) {
+			break
+		}
+
+		index++
+	}
+
+	return sortedModules, requirements
 }
 
 func (c *Core) stopModules(modules []Module) {
