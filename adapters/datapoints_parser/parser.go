702d0a5a9f3f87b8754521fe543059be6bf2adc3
alekseiKromski <aleksei.kromski@outlook.com>
2024-02-13 23:40:51 +0200

    parser: reworked the idea of parser
    Currently, we need to create it once and then pass data as argument
    Added deviceUuid as return parameter

--- adapters/datapoints_parser/parser.go
+++ adapters/datapoints_parser/parser.go
@@ -8,71 +8,71 @@ import (
 	"time"
 )
 
-type DataPointsParser struct {
-	Datapoints []models.DataPoints
-}
+type DataPointsParser struct{}
 
 func NewDataPointsParser() *DataPointsParser {
-	return &DataPointsParser{
-		Datapoints: []models.DataPoints{},
-	}
+	return &DataPointsParser{}
 }
 
-// TIME::2019-10-12T07:20:50.52Z;TEMP::14;PRS::1000PA
-func (dpp *DataPointsParser) Parse(data string) error {
+// DEVICE::3cc76ff4-cbaa-436c-b727-45d526facfc7;TIME::2019-10-12T07:20:50.52Z;TEMP::14C;PRS::1000PA
+func (dpp *DataPointsParser) Parse(data string) (string, []models.DataPoints, error) {
+	var datapoints []models.DataPoints
+	incomingDatapoints := dpp.parseStringToMap(data)
 
-	dataPoints := dpp.parseStringToMap(data)
+	deviceUuid := incomingDatapoints["DEVICE"]
 
-	measurementTime, err := time.Parse(time.RFC3339, dataPoints["TIME"])
+	measurementTime, err := time.Parse(time.RFC3339, incomingDatapoints["TIME"])
 	if err != nil {
-		return fmt.Errorf("cannot parse time in data: %s. Reason: %v", data, err)
+		return deviceUuid, nil, fmt.Errorf("cannot parse time in data: %s. Reason: %v", data, err)
 	}
 
-	for key, val := range dataPoints {
+	for key, val := range incomingDatapoints {
 		switch key {
-		case "TIME":
+		case "TIME": // Measurement timestamp
+			continue
+		case "DEVICE": // Device uuid
 			continue
 		case "TEMP":
 			td := &models.TemperatureData{}
 			if err := td.ParseFromString(val, measurementTime); err != nil {
-				return fmt.Errorf("cannot parse temperature string: %v", err)
+				return deviceUuid, nil, fmt.Errorf("cannot parse temperature string: %v", err)
 			}
 
-			dpp.Datapoints = append(dpp.Datapoints, td)
+			datapoints = append(datapoints, td)
 		case "GEO":
 			gd := &models.GeoDataPoint{}
 			if err := gd.ParseFromString(val, measurementTime); err != nil {
-				return fmt.Errorf("cannot parse geo string: %v", err)
+				return deviceUuid, nil, fmt.Errorf("cannot parse geo string: %v", err)
 			}
 
-			dpp.Datapoints = append(dpp.Datapoints, gd)
+			datapoints = append(datapoints, gd)
 		case "PRS":
 			pd := &models.PressureData{}
 			if err := pd.ParseFromString(val, measurementTime); err != nil {
-				return fmt.Errorf("cannot parse pressure string: %v", err)
+				return deviceUuid, nil, fmt.Errorf("cannot parse pressure string: %v", err)
 			}
 
-			dpp.Datapoints = append(dpp.Datapoints, pd)
+			datapoints = append(datapoints, pd)
 		case "ALT":
 			ad := &models.AltitudeData{}
 			if err := ad.ParseFromString(val, measurementTime); err != nil {
-				return fmt.Errorf("cannot parse altitude string: %v", err)
+				return deviceUuid, nil, fmt.Errorf("cannot parse altitude string: %v", err)
 			}
 
-			dpp.Datapoints = append(dpp.Datapoints, ad)
+			datapoints = append(datapoints, ad)
 		case "HUM":
 			hd := &models.HumidityData{}
 			if err := hd.ParseFromString(val, measurementTime); err != nil {
-				return fmt.Errorf("cannot parse humidity string: %v", err)
+				return deviceUuid, nil, fmt.Errorf("cannot parse humidity string: %v", err)
 			}
 
-			dpp.Datapoints = append(dpp.Datapoints, hd)
+			datapoints = append(datapoints, hd)
 		default:
 			log.Printf("cannot parse key-value: %s/%s. Incoming string: %s", key, val, data)
 		}
 	}
 
-	return nil
+	return deviceUuid, datapoints, nil
 }
 
 func (dpp *DataPointsParser) parseStringToMap(data string) map[string]string {
