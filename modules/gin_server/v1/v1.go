61e9b61ad4473d0965b25dae019c06236de16267
alekseiKromski <aleksei.kromski@outlook.com>
2024-03-03 00:57:39 +0200

    gin: added basic endpoint to return all information

--- modules/gin_server/v1/v1.go
+++ modules/gin_server/v1/v1.go
@@ -2,7 +2,9 @@ package v1
 
 import (
 	"alekseikromski.com/atlanta/modules/storage"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
 type V1 struct {
@@ -18,8 +20,16 @@ func NewV1Api(storage storage.Storage) *V1 {
 }
 
 func (v *V1) RegisterRoutes() error {
-
+	v.router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"PUT", "PATCH", "GET"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 	v.router.GET("/healthz", v.Healthz)
+	v.router.GET("/datapoint/get-all", v.GetAllDatapoints(v.storage))
 	users := v.router.Group("/users")
 	{
 		users.POST("/create", v.CreateUser(v.storage))
