f02e17be6aa30bf3556a13724e058a145269dcee
alekseiKromski <aleksei.kromski@outlook.com>
2024-03-03 00:57:14 +0200

    storage: added new interface for datapoints

---
+++ modules/storage/datapoints.go
@@ -0,0 +1,21 @@
+package storage
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type Datapoint struct {
+	ID              uuid.UUID `json:"id"`
+	DeviceId        uuid.UUID `json:"device_id"`
+	Value           string    `json:"value"`
+	ValueType       string    `json:"type"`
+	Unit            string    `json:"unit"`
+	MeasurementTime time.Time `json:"measurement_time"`
+	CreatedAt       time.Time `json:"created_at"`
+	UpdatedAt       time.Time `json:"updated_at"`
+}
+
+type StorageDatapoint interface {
+	GetAllDatapoints() ([]*Datapoint, error)
+}
--- modules/storage/storage.go
+++ modules/storage/storage.go
@@ -5,5 +5,6 @@ import "alekseikromski.com/atlanta/models"
 type Storage interface {
 	SaveDatapoints(deviceUuid string, datapoints []models.DataPoints) error // SaveDatapoints - Save datapoint to database
 
-	StorageUser // StorageUser - crud interface for working with users
+	StorageUser      // StorageUser - crud interface for working with users
+	StorageDatapoint // StorageDatapoint - all commands related to datapoints
 }
