2bd7cad36016ccb64f1a691676460729234ed5e3
alekseiKromski <aleksei.kromski@outlook.com>
2024-03-04 22:43:34 +0200

    humidity: revert changes

--- models/HumidityData.go
+++ models/HumidityData.go
@@ -7,20 +7,20 @@ import (
 )
 
 type HumidityData struct {
-	Humidity *IntegerData `json:"humidity"`
-	Unit     string       `json:"unit"`
-	Time     time.Time    `json:"time"` //Measurement time
+	Humidity *FloatData `json:"humidity"`
+	Unit     string     `json:"unit"`
+	Time     time.Time  `json:"time"` //Measurement time
 }
 
 func (h *HumidityData) ParseFromString(val string, measurementTime time.Time) error {
-	value, err := strconv.ParseInt(val, 0, 64)
+	value, err := strconv.ParseFloat(val, 64)
 	if err != nil {
-		return fmt.Errorf("cannot parse HUM in string: %s. Reason: %v", val, err)
+		return fmt.Errorf("cannot parse PRS in string: %s. Reason: %v", val, err)
 	}
 
-	h.Humidity = &IntegerData{
-		Value: value,
-		Type:  "int",
+	h.Humidity = &FloatData{
+		Value: float64(value),
+		Type:  "float",
 	}
 	h.Unit = "percentage"
 	h.Time = measurementTime
@@ -30,7 +30,7 @@ func (h *HumidityData) ParseFromString(val string, measurementTime time.Time) er
 
 func (h *HumidityData) ToArguments() []any {
 	return []any{
-		strconv.FormatInt(h.Humidity.Value, 2),
+		strconv.FormatFloat(h.Humidity.Value, 'f', 6, 64),
 		h.Humidity.Type,
 		h.Unit,
 		h.Time.Format(time.RFC3339),
